1391d58e3bdd4e1e3b82e3e82823ae841cebcc6d
Abdullah <abdullasaad28@gmail.com>
2023-03-17 13:04:56 -0700

    r

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
-#include <stdlib. .h>
-#include <time. h>
-#include <stdio. h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
  int main(void)
          int n;
          srand(time(0));
