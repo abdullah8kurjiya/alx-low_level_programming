f3d849ec6c8f832606a7681404911c7357252975
abdullah8kurjiya <abdullasaad28@gmail.com>
2023-10-30 17:06:13 -0700

    fix betty

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,49 +1,46 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
-* argstostr - concatenates all the arguments of your program.
-* @ac: arguments count
-* @av: arguments vector
-*
-* Return: a pointer to a new string, or NULL if it fails
-*/
+ * argstostr - concatenates all the arguments of your program
+ *@ac: number of arguments
+ *@av: arguments
+ * Return: a pointer to a new string
+ */
 char *argstostr(int ac, char **av)
 {
-  char *str, *s;
-  int i, j, k, len = 0;
+	int i;
+	int j;
+	char *p = NULL;
+	int k;
+	int ext;
 
-  if (ac == 0 || av == NULL)
-     return (NULL);
+	k = 0;
+	ext = 0;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			ext++;
+		}
+	}
 
-  for (i = 0; i < ac; i++)
-  {
-     s = av[i];
-     j = 0;
-
-     while (s[j++])
-        len++;
-     len++;
-  }
-
-  str = (char *)malloc(sizeof(char) * (len + 1));
-  if (str == NULL)
-     return (NULL);
-
-  for (i = 0, j = 0; i < ac && j < len; i++)
-  {
-     s = av[i];
-     k = 0;
-
-     while (s[k])
-     {
-        str[j] = s[k];
-        k++;
-        j++;
-     }
-     str[j++] = '\n';
-  }
-  str[j] = '\0';
-
-  return (str);
+	p = (char *)malloc(ext + ac + 1 * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			p[k] = av[i][j];
+			k++;
+		}
+		p[k] = '\n';
+		k++;
+	}
+	p[k] = '\0';
+	return (p);
 }
